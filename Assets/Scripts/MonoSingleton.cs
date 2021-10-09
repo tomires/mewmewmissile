26e6f8729393918742e9e9a86d215e38ac57c13a
tomires <me@tomires.eu>
2021-10-09 20:38:39 +0200

    Add models, sounds

---
+++ Assets/Scripts/MonoSingleton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = (T)this;
+        }
+    }
+}
